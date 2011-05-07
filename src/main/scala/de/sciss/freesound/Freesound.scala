9057f1697b27d0a61469e24f6efa0435ed38765e
Sciss <contact@sciss.de>
2011-05-07 11:54:44 +0200

    ...

--- src/main/scala/de/sciss/freesound/Freesound.scala
+++ src/main/scala/de/sciss/freesound/Freesound.scala
@@ -36,8 +36,8 @@ import actors.{Actor, OutputChannel, Future, DaemonActor}
  */
 object Freesound {
    val name          = "ScalaFreesound"
-   val version       = 0.10
-   val copyright     = "(C)opyright 2010 Hanns Holger Rutz"
+   val version       = 0.11
+   val copyright     = "(C)opyright 2010-2011 Hanns Holger Rutz"
 
    def versionString = (version + 0.001).toString.substring( 0, 4 )
 
